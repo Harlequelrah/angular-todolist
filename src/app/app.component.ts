93c907fdcef3bc1e7d63524d8db9eac4f8375c9e
Harlequelrah <degbovimax19@gmail.com>
2025-01-28 18:54:08 +0000

    debut d'utilisation des pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/components/header/header.component';
-import { NgIf } from '@angular/common';
+import { AsyncPipe, NgIf } from '@angular/common';
+import { interval, Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,NgIf],
+  imports: [RouterOutlet, HeaderComponent, NgIf,AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   showHeader: boolean = true;
+  interval$! : Observable<number>;
   constructor(private router: Router) {
     this.router.events.subscribe(
       () => {
         this.showHeader = this.router.url !== '/' && this.router.url!=='/register';
       }
     )
+  }
+  ngOnInit(): void {
+    this.interval$ = interval(1000);
   }
 }
