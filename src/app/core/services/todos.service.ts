088d62ee0c32ef7bab9ab05605aae5134033e3a8
Harlequelrah <degbovimax19@gmail.com>
2025-02-03 14:24:17 +0000

    endpoint pour la creation et le retour d'une todo

--- src/app/core/services/todos.service.ts
+++ src/app/core/services/todos.service.ts
@@ -14,79 +14,17 @@ export class TodosService{
 
     constructor(private http: HttpClient) { }
 
-    
-private todos = [
-    new Todo(
-        1,
-        "Learning Angular",
-        "I am now learning angular",
-        true,
-        new Date(),
-        new Date(),
-    ),
-    new Todo(
-        2,
-        "Strike The blood",
-        "Finir l'animé Strike The blood",
-        false,
-        new Date(),
-        new Date()
-    ),
-    new Todo(
-        3,
-        "Continue TOEIC",
-        "End my TOEIC courses in Global Exam",
-        true,
-        new Date()
-    ),
-    new Todo(
-        4,
-        "Learning TypeScript",
-        "I am now learning angular",
-        false,
-        new Date(),
-        new Date(),
-    ),
-    new Todo(
-        5,
-        "Bleach",
-        "Finir l'animé Bleach",
-        true,
-        new Date(),
-        new Date()
-    ),
-    new Todo(
-        6,
-        "Apprendre Spring boot",
-        "End my TOEIC courses in Global Exam",
-        false,
-        new Date()
-    )
 
 
-]
     getTodos(): Observable<Todo[]> {
         return this.http.get<Todo[]>(`${this.baseUrl}/read-all`);
     }
-    getTodo(id:number): Todo{
-        const todo = this.todos.find(todo => todo.id == id);
-        if (!todo) {
-            throw new Error("Todo Not Found")
-        }
-        return todo;
-
+    getTodo(id: number): Observable<Todo>{
+        return this.http.get<Todo>(`${this.baseUrl}/read-one/${id}`);
     }
-    addTodo(formValue:{titre:string,description:string}): void
+    addTodo(formValue:{titre:string,description:string}): Observable<Todo>
     {
-        const todo: Todo = new Todo(
-            this.todos[this.todos.length - 1].id + 1,
-            formValue.titre,
-            formValue.description,
-            false,
-            new Date(),
-            new Date(),
-        );
-        this.todos.push(todo);
+        return this.http.post<Todo>(`${this.baseUrl}/create`,formValue);
     }
 
 }
--- src/app/todos/components/single-todo/single-todo.component.ts
+++ src/app/todos/components/single-todo/single-todo.component.ts
@@ -1,13 +1,14 @@
-import { DatePipe, NgStyle, TitleCasePipe } from '@angular/common';
+import { AsyncPipe, DatePipe, NgIf, NgStyle, TitleCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Todo } from '../../../core/models/todo';
 import { TodosService } from '../../../core/services/todos.service';
+import { map, Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-single-todo',
   standalone: true,
-  imports: [NgStyle,TitleCasePipe,DatePipe],
+  imports: [NgStyle,TitleCasePipe,DatePipe,AsyncPipe,NgIf],
   templateUrl: './single-todo.component.html',
   styleUrl: './single-todo.component.scss'
 })
@@ -15,27 +16,16 @@ export class SingleTodoComponent {
   constructor(private todoService: TodosService, private route: ActivatedRoute) { }
 
 
-  todo!: Todo;
-  button_text!: "Commencer" | "Finir";
+  todo$!: Observable<Todo>;
   ngOnInit(): void {
-
     this.getTodo();
-    this.button_text = this.todo.est_fini ? "Commencer" : "Finir"
   }
-  OnChangeStatut(): void {
-    this.todo.est_fini = !this.todo.est_fini
-    if (this.todo.est_fini) {
-      this.button_text = "Commencer"
-    }
-    else {
-      this.button_text = "Finir"
-    }
-  }
+
   private getTodo(): void {
     const todoId = Number(this.route.snapshot.params['id']);
     if (todoId) {
       console.log(todoId);
-      this.todo = this.todoService.getTodo(todoId);
+      this.todo$ = this.todoService.getTodo(todoId);
     }
 
   }
